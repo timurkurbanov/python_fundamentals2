43522acf8e9053424eb6c05ddf802c301bae2e82
TIMUR <1tkurbanov@gmail.com>
2019-05-07 16:59:00 -0400

    6th

--- six.py
+++ six.py
@@ -1,4 +1,4 @@
 def wrap_text(message, wrapper):
     return wrapper + message + wrapper
 
-print(wrap_text(wrap_text(wrap_text("hi", "-------"), "==="), "---"))
+print(wrap_text(wrap_text(wrap_text("hi", "----"), "===="), "--"))
